a44d58470f8d4c1594eb925547475f6f9d425379
hanzalaahmeddin <hanzala.ahmed.promos@gmail.com>
2025-07-01 11:45:49 +0500

    new code push

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Header'
+import First from './First'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +12,7 @@ function App() {
     <>
       <h1>Hello React App</h1>
       <Header />
-      <h2>dsds</h2>
+      <First />
     </>
   )
 }
---
+++ src/First.jsx
@@ -0,0 +1,14 @@
+function First() {
+    return (
+        <div>
+            <h2>This is Fisrt Component</h2>
+            <Fruit />
+        </div>
+    )
+}
+function Fruit() {
+    return (
+        <h2>This is Apple</h2>
+    )
+}
+export default First;
